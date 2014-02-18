cbb372b51823a41d878ad6d54f6e6b2df1b93ceb
Morgan Kleene <morgan.kleene@mandiant.com>
2014-02-17 23:40:49 -0500

    webapp for marking stuff

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,50 @@
-var app = require('express')();
+var express = require('express'), 
+    app = express.createServer(), 
+    path = require('path'),
+    fs = require('fs'),
+    IMAGE_DIR = path.join(__dirname, 'images'),
+    JS_DIR = path.join(__dirname, 'static'),
+    PORT = 8081;
 
-app.get('/', function(req, res){
+app.use(express.bodyParser());
+
+app.get('/image', function(req, res) {
+    var files = fs.readdirSync(IMAGE_DIR).filter(function (f) { return /\.jpg$/.test(f); });
+    return res.json(files);
 });
 
-app.listen(3000);
+app.get('/:image/metadata', function (req, res) {
+    var imageName = req.params.image;
+
+    fs.readFile(path.join(IMAGE_DIR, imageName + '.json'), 'utf8', function (err, data) {
+        if (err && err.code === 'ENOENT') {
+            return res.json({}, 200);
+        }
+        if (err) { 
+            console.log(err);
+            return res.send('There was an error', { 'Content-type': 'text/plain' }, 500);    
+        }
+
+        return res.json(JSON.parse(data), 200);
+    });
+});
+
+app.put('/:image/metadata', function (req, res) {
+    var imageName = req.params.image, data = req.body;
+
+    fs.writeFile(path.join(IMAGE_DIR, imageName + '.json'), JSON.stringify(data), 'utf8', function (err) {
+        if (err) {
+            console.log(err);
+            return res.send('There was an error saving.', { 'Content-type': 'text/plain' }, 500);
+        } 
+
+        return res.send('OK', {'Content-type': 'text/plain'}, 200);
+    }); 
+});
+
+app.use(express.static(IMAGE_DIR));
+app.use(express.static(JS_DIR));
+app.use(app.router);
+
+
+app.listen(PORT);
---
+++ webapp/static/clientapp.js
@@ -0,0 +1,128 @@
+angular.module('imageMarker', [])
+    .config(['$locationProvider', function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }])
+    .controller('ImageListCtrl', ['$scope', '$window', '$http', '$location', function ($scope, $wndw, $http, $location) {
+        var images, currentIndex, loadedMetadataFor, startCropPosition;
+
+        function setIndex(index) {
+            if (index < 0 || index > images.length) return;
+
+            var image = images[index];
+
+            currentIndex = index;
+            $scope.image = image;
+            
+            loadedMetadataFor = null;
+            $http({ method: 'GET', url: '/' + encodeURIComponent($scope.image) + '/metadata' }).
+                success(function (data) {
+                    loadedMetadataFor = image;
+                    $scope.metadata = data;
+                    if (!('isWatch' in $scope.metadata)) {
+                        $scope.metadata.isWatch = true;
+                    }
+                });
+        }
+
+        function saveMetadata() {
+            $http.put('/' + encodeURIComponent($scope.image) + '/metadata', JSON.stringify($scope.metadata), 
+                    { 'Content-type': 'application/json' })
+                .error(function () {
+                    console.error('There was an error saving the metadata');
+                });
+        }
+
+        function getPositionInImage($event) {
+            var image = $wndw.document.getElementById('theimage'),
+                clientRect = image.getBoundingClientRect(),
+                iTop = $wndw.scrollX + clientRect.top,
+                iLeft = $wndw.scrollY + clientRect.left;
+
+            return {
+                x: $event.pageX - iLeft,
+                y: $event.pageY - iTop
+            };
+
+        }
+
+        function markPosition(position) {
+            $scope.metadata.center = position;
+            saveMetadata();
+        }
+
+        function getNormalizedRect(p1, p2) {
+            var topLeft = { x: Math.min(p1.x, p2.x), y: Math.min(p1.y, p2.y) },
+                bottomRight =  { x: Math.max(p1.x, p2.x), y: Math.max(p1.y, p2.y) };
+
+
+            return {
+                topLeft: topLeft,
+                width: bottomRight.x - topLeft.x,
+                height: bottomRight.y - topLeft.y
+            };
+        }
+       
+
+        $scope.prefix = $location.search().p;
+
+        $scope.goNext = function () {
+            var index = currentIndex + 1;
+
+            while (currentIndex < images.length && ($scope.prefix && images[index].indexOf($scope.prefix) !== 0)) {
+                index++;
+            }
+            setIndex(index);
+        };
+
+        $scope.goPrevious = function () {
+            var index = currentIndex - 1;
+
+            while (currentIndex >= 0 && ($scope.prefix && images[index].indexOf($scope.prefix) !== 0)) {
+                index--;
+            }
+            setIndex(index);
+        };
+
+        $scope.markWatch = function (image, isWatch) {
+            $scope.metadata.isWatch = isWatch;
+            saveMetadata();
+        };
+
+        $scope.loaded = function () {
+            return loadedMetadataFor === $scope.image;
+        };
+
+        $scope.mousedown = function ($event) {
+            startCropPosition = getPositionInImage($event);
+        };
+
+        $scope.mouseup = function ($event) {
+            var endCropPosition = getPositionInImage($event),
+                distFromStart = Math.sqrt(Math.pow(startCropPosition.x - endCropPosition.x, 2) +
+                                          Math.pow(startCropPosition.y - endCropPosition.y, 2));
+
+            if (distFromStart < 10) {
+                markPosition(startCropPosition);
+                startCropPosition = void 0;
+                return;
+            }
+
+            $scope.metadata.crop = getNormalizedRect(startCropPosition, endCropPosition);
+            saveMetadata();
+
+            startCropPosition = void 0;
+        };
+
+        $scope.mousemove = function ($event) {
+            if (startCropPosition === void 0) return;
+
+            $scope.metadata.crop = getNormalizedRect(startCropPosition, getPositionInImage($event));
+        };
+
+        $http({ method: 'GET', url: '/image'}).
+            success(function(data) {
+                images = data;
+                currentIndex = -1;
+                $scope.goNext();
+            });
+    }]);
